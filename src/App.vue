872049e4524f9e7de150b0a3e8722628309f871e
iKurisu <chris.epic01@gmail.com>
2019-03-23 16:58:20 +0100

    Add dispatch when instance is created

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,15 @@ import store from './store';
 export default {
   name: "App",
   store,
+  created() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        store.dispatch('setData', position)
+      })
     }
+  },
+  computed: {
+    message: () => store.state.location ? 'We know were you live...' : 'Please activate location'
   }
 }
 </script>
