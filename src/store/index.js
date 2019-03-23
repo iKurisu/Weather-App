cf2d3ad3c12809df2191644eab1a85af46e4890e
iKurisu <chris.epic01@gmail.com>
2019-03-23 16:56:33 +0100

    Add action to store weather data

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { fetchByCoords } from '../api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    location: null
+    location: null,
+    forecast: []
   },
   mutations: {
     setLocation(state, location) {
       state.location = location;
+    },
+    setForecast(state, forecast) {
+      state.forecast = forecast
+    }
+  },
+  actions: {
+    async setData({ commit }, location) {
+      const data = await fetchByCoords(location.coords);
+      commit('setLocation', location);
+      commit('setForecast', data.data.list)
     }
   }
 })
