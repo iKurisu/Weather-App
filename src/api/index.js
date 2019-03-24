d0d38eea317fdf3d69af2a32ba4f1c91adba14cf
iKurisu <chris.epic01@gmail.com>
2019-03-24 09:21:21 +0100

    Add functions to fetch weather and forecast

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { getWeatherAtNoon } from '../utils';
 
-export const fetchByCoords = async ({ latitude: lat, longitude: lon }) => {  
-  try {
-    const url = "https://api.openweathermap.org/data/2.5/forecast";
-    const APPID = "e63619f529ef55f19299184ab165dcd9";
+const APPID = "e63619f529ef55f19299184ab165dcd9";
+const url = "https://api.openweathermap.org/data/2.5"
 
 const handleError = fn => (...params) => fn(...params).catch(console.error);
 
@@ -16,9 +14,17 @@ const makeRequest = async (url, params) => {
 const byCoords = url => ({ latitude: lat, longitude: lon }) => {
   const params = { lat, lon, APPID };
   return makeRequest(url, params);
-  }
+}
 
 const byCity = url => city => {
   const params = { city, APPID };
   return makeRequest(url, params);
 }
+
+const fetchWeather = fn => handleError(fn(`${url}/weather`));
+const fetchForecast = fn => handleError(fn(`${url}/forecast`));
+
+export const fetchWeatherByCoords = fetchWeather(byCoords);
+export const fetchForecastByCoords = fetchForecast(byCoords);
+export const fetchWeatherByCity = fetchWeather(byCity);
+export const fetchForecastByCity = fetchForecast(byCity);
