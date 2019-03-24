de4f1e07e51f198c2ef16601142952bc5abfaf70
iKurisu <chris.epic01@gmail.com>
2019-03-24 17:08:20 +0100

    Add utilities to convert temperature

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,6 @@ export const getForecastAtNoon = data => {
   const { list: forecasts } = data.data;
   return forecasts.filter(forecast => getHour(forecast.dt_txt) === 12);
 }
+
+export const toCelsius = t => Math.floor(t - 273.15);
+export const toFahrenheit = t => toCelsius(t) * 9/5 + 32;
