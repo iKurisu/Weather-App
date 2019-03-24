b3fc5ae4cc831e61263d2543541970fc4fa20b0b
iKurisu <chris.epic01@gmail.com>
2019-03-24 09:19:07 +0100

    Add error handler

--- src/api/index.js
+++ src/api/index.js
@@ -6,9 +6,7 @@ export const fetchByCoords = async ({ latitude: lat, longitude: lon }) => {
     const url = "https://api.openweathermap.org/data/2.5/forecast";
     const APPID = "e63619f529ef55f19299184ab165dcd9";
 
-    const response = await axios.get(url, {
-      params: { lat, lon, APPID }
-    });
+const handleError = fn => (...params) => fn(...params).catch(console.error);
 
 const makeRequest = async (url, params) => {
   const response = await axios.get(url, { params });
