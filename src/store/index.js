2897482d1e5ab8ad7ee2a193559f9fa4eb015e99
iKurisu <chris.epic01@gmail.com>
2019-03-29 18:30:50 +0100

    Split store into modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,85 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { 
-  fetchForecastByCoords, 
-  fetchWeatherByCoords,
-  fetchForecastByCity,
-  fetchWeatherByCity 
-} from '../api';
-import { getForecastAtNoon } from '../utils';
+import form from './modules/form';
+import weather from './modules/weather';
+import place from './modules/place';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    location: null,
-    weather: null,
-    forecasts: [],
-    places: [],
-    formIsActive: false,
-    inputIsValid: false
-  },
-  mutations: {
-    setLocation(state, location) {
-      state.location = location;
-    },
-    setWeather(state, weather) {
-      state.weather = weather;
-    },
-    setForecasts(state, forecasts) {
-      state.forecasts = forecasts
-    },
-    setPlace(state, place) {
-      state.location = place;
-    },
-    addPlace(state, [ city, code ]) {
-      const id = new Date().getTime();
-      state.places.push({ 
-        id,
-        place: [ city, code ], 
-      });
-    },
-    toggleForm(state) {
-      state.formIsActive = !state.formIsActive;
-    },
-    setInput(state, bool) {
-      state.inputIsValid = bool;
-    }
-  },
-  actions: { 
-    storeData({ commit }, { weather, forecasts }) {
-      if (weather) {
-        commit('setForecasts', getForecastAtNoon(forecasts));
-        commit('setWeather', weather);
-      }
-    },
-    async setData({ commit, dispatch }, location) {
-      commit('setLocation', location);
-      const weather = await fetchWeatherByCoords(location.coords);
-      const forecasts = await fetchForecastByCoords(location.coords);
-      dispatch('storeData', { weather, forecasts });
-    },
-    async addPlace({ commit, dispatch }, place) {
-      commit('addPlace', place);
-      const weather = await fetchWeatherByCity(place);
-      const forecasts = await fetchForecastByCity(place);
-
-      if (weather && forecasts) {
-        commit('setInput', true);
-        commit('toggleForm');
-        dispatch('storeData', { weather, forecasts });
-      } else {
-        commit('setInput', false);
-      }
-    },
-    async setPlace({ commit, dispatch }, place) {
-      commit('setPlace', place);
-      const weather = await fetchWeatherByCity(place);
-      const forecasts = await fetchForecastByCity(place);
-
-      if (weather && forecasts) {
-        dispatch("storeData", { weather, forecasts });
-      }
-    }
-  }
+  modules: {
+    form,
+    weather,
+    place
+  }, 
 })
