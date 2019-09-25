7b00732e1ecf8da06c23877cbc9f39a795ee8db8
iKurisu <chris.epic01@gmail.com>
2019-09-25 16:39:24 +0200

    Split into smaller objects

--- src/store/modules/weather.js
+++ src/store/modules/weather.js
@@ -7,52 +7,66 @@ import {
 import { getForecastAtNoon, forecastFormat, nextThreeDays } from "../../utils";
 import * as convert from "../../utils/temperature";
 
-const setWeather = (weatherFn, forecastFn) => async ({ commit }, place) => {
-  const currentWeather = await weatherFn(place);
-  const forecasts = await forecastFn(place);
+const state = {
+  currentWeather: null,
+  forecasts: [],
+  unit: "Celsius"
+};
+
+const getters = {
+  city({ currentWeather }) {
+    if (currentWeather) return currentWeather.name;
+  },
+  countryCode({ currentWeather }) {
+    if (currentWeather) return currentWeather.sys.country;
+  },
+  currentWeather({ currentWeather, unit }) {
+    if (currentWeather) {
+      return {
+        temperature: convert[`to${unit}`](currentWeather.main.temp_map),
+        description: currentWeather.weather[0].description,
+        main: currentWeather.weather[0].main.toLowerCase()
+      };
+    }
+  },
+  forecasts({ currentWeather, forecasts, unit }) {
+    if (currentWeather) {
+      return [currentWeather, ...forecasts.filter(nextThreeDays)].map(
+        forecastFormat(unit)
+      );
+    }
+
+    return [];
+  }
+};
+
+const mutations = {
+  setCurrentWeather(state, currentWeather) {
+    state.currentWeather = currentWeather.data;
+  },
+  setForecasts(state, forecasts) {
+    state.forecasts = getForecastAtNoon(forecasts);
+  },
+  changeUnits(state, unit) {
+    state.unit = unit;
+  }
+};
+
+const setWeather = (getWeather, getForecast) => async ({ commit }, place) => {
+  const currentWeather = await getWeather(place);
+  const forecasts = await getForecast(place);
   commit("setWeather", { currentWeather, forecasts });
+};
+
+const actions = {
+  setWeatherFromCoords: setWeather(fetchWeatherByCoords, fetchForecastByCoords),
+  setWeatherFromCity: setWeather(fetchWeatherByCity, fetchForecastByCity)
 };
 
 export default {
   namespaced: true,
-  state: {
-    currentWeather: null,
-    forecasts: [],
-    unit: "Celsius"
-  },
-  getters: {
-    city: ({ currentWeather }) => currentWeather && currentWeather.name,
-    countryCode: ({ currentWeather }) =>
-      currentWeather && currentWeather.sys.country,
-    currentWeather: ({ currentWeather, unit }) => ({
-      temperature:
-        currentWeather && convert[`to${unit}`](currentWeather.main.temp_max),
-      description: currentWeather && currentWeather.weather[0].description,
-      main: currentWeather && currentWeather.weather[0].main.toLowerCase()
-    }),
-    forecasts: ({ currentWeather, forecasts, unit }) => {
-      if (currentWeather) {
-        return [currentWeather, ...forecasts.filter(nextThreeDays)].map(
-          forecastFormat(unit)
-        );
-      }
-      return [];
-    }
-  },
-  mutations: {
-    setWeather(state, { currentWeather, forecasts }) {
-      state.currentWeather = currentWeather.data;
-      state.forecasts = getForecastAtNoon(forecasts.data);
-    },
-    changeUnits(state, unit) {
-      state.unit = unit;
-    }
-  },
-  actions: {
-    setWeatherFromCoords: setWeather(
-      fetchWeatherByCoords,
-      fetchForecastByCoords
-    ),
-    setWeatherFromCity: setWeather(fetchWeatherByCity, fetchForecastByCity)
-  }
+  state,
+  getters,
+  mutations,
+  actions
 };
